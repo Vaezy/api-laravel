94abeba3a32fb00ffc4b360bc556912b812a6faf
Vaezy <boy1.thoomas@gmail.com>
2026-02-11 12:11:00 +0100

    feat: add activity 5

--- routes/api.php
+++ routes/api.php
@@ -15,16 +15,16 @@ Route::get('/ping', function () {
     ]);
 });
 
-Route::post('/register', [UserController::class, 'register']);
-Route::post('/login', [UserController::class, 'login'])->middleware('throttle:10,1');
+Route::post('/register', [UserController::class, 'register'])->name('auth.register');
+Route::post('/login', [UserController::class, 'login'])->middleware('throttle:10,1')->name('auth.login');
 
-Route::get('/books', [BookController::class, 'index']);
-Route::get('/books/{book}', [BookController::class, 'show']);
+Route::get('/books', [BookController::class, 'index'])->name('books.index');
+Route::get('/books/{book}', [BookController::class, 'show'])->name('books.show');
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/logout', [UserController::class, 'logout']);
+    Route::post('/logout', [UserController::class, 'logout'])->name('auth.logout');
 
-    Route::post('/books', [BookController::class, 'store']);
-    Route::match(['put','patch'], '/books/{book}', [BookController::class, 'update']);
-    Route::delete('/books/{book}', [BookController::class, 'destroy']);
+    Route::post('/books', [BookController::class, 'store'])->name('books.store');
+    Route::match(['put','patch'], '/books/{book}', [BookController::class, 'update'])->name('books.update');
+    Route::delete('/books/{book}', [BookController::class, 'destroy'])->name('books.destroy');
 });
