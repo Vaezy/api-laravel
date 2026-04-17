c5d7c2b3f832880a4d0a59cc7f55a3e3d507e179
Vaezy <boy1.thoomas@gmail.com>
2026-04-17 16:37:22 +0200

    chore: implement automatic database seeding for pokemons

--- src/db/db.js
+++ src/db/db.js
@@ -1,8 +1,28 @@
 import mongoose from "mongoose";
+import Pokemon from "../models/pokemon-model.js";
+import pokemons from "./mock-pokemon.js";
 
 export const initDb = () => {
-    mongoose
-        .connect("mongodb://localhost/pokemon-api-rest")
-        .then(() => console.log("✅ Connexion à MongoDB réussie !"))
-        .catch((err) => console.error("❌ Erreur de connexion :", err));
+  const connectionString = "mongodb://127.0.0.1:27017/pokedex";
+
+  mongoose
+    .connect(connectionString)
+    .then(async () => {
+      console.log("✅ Connexion à MongoDB réussie !");
+
+      const count = await Pokemon.countDocuments();
+
+      if (count === 0) {
+        console.log("La base est vide, insertion des pokémons...");
+
+        await Pokemon.insertMany(pokemons);
+
+        console.log("✅ Pokémons insérés avec succès !");
+      } else {
+        console.log(`La base contient déjà ${count} pokémons.`);
+      }
+    })
+    .catch((error) => {
+      console.error("Erreur de connexion :", error);
+    });
 };
